20d9206f62e796ee36e9eaeaa88080c6fe278320
Lorena Mendes Barbosa <75842072+LorenaMendesB@users.noreply.github.com>
2022-04-30 22:06:37 -0300

    responsive search

--- src/components/Search/styled.js
+++ src/components/Search/styled.js
@@ -3,25 +3,28 @@ export const AreaSearch = styled.div`
 position: absolute;
 left: 330px;
 top: 21px;
-@media (min-width: 1764px){
 input{
     position: absolute;
-    width: 450px;
-    height: 45px;
-    left: 34px;
-    top: 21px;
-    background: #D9D9D9;
-    border-radius: 61px;
-    padding: 0px 45px;
-    border: none;
     font-family: 'Roboto';
     font-style: normal;
     font-weight: 900;
+    background: #D9D9D9;
+    border-radius: 61px;
+    padding: 0px 45px;
     font-size: 15px;
     line-height: 18px;
     &:hover{
         border: none;
     }
+    
+}
+.bi{
+position: absolute;
+left: 45px;
+top: 31px;
+bottom: 94.81%;
+font-size: 25px;
+
 }
 #Search::-webkit-search-cancel-button {
     position: relative;
@@ -32,7 +35,7 @@ input{
     width: 15px;
     left: 20px;
  
-  }
+}
 #Search::placeholder {
     
     font-family: 'Roboto';
@@ -47,43 +50,62 @@ input{
 
 }
 .bi{
-position: absolute;
-left: 45px;
-top: 31px;
-bottom: 94.81%;
-font-size: 25px;
-background: #5E5656;
-color: #5E5656;
+    position: absolute;
+    background: #5E5656;
+    color: #5E5656;
     &:hover{
         color: #9A9191;
     }
 }
+@media (min-width: 1764px){
+input{
+    
+    width: 450px;
+    height: 45px;
+    left: 34px;
+    top: 21px;
+   
+}
+#Search::-webkit-search-cancel-button {
+    
+    height: 15px;
+    width: 15px;
+    left: 20px;
+ 
+  }
+#Search::placeholder {
+    
+    font-size: 15px;
+    line-height: 18px;
+    margin-left: 20px;
+ 
+
+}
+.bi{
+
+left: 45px;
+top: 31px;
+bottom: 94.81%;
+font-size: 25px;
+
+
+}
 }
 @media (max-width: 1763px){
     input{
-        position: absolute;
+       
         width: 450px;
         height: 45px;
         left: 14px;
         top: 21px;
-        background: #D9D9D9;
-        border-radius: 61px;
-        padding: 0px 45px;
-        border: none;
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 900;
+        
         font-size: 15px;
         line-height: 18px;
-        &:hover{
-            border: none;
-        }
+       
     }
     #Search::-webkit-search-cancel-button {
         position: relative;
-        background-image: url(cancel.svg);
-        background-repeat: no-repeat;
-        -webkit-appearance: none;
+     
         height: 15px;
         width: 15px;
         left: 20px;
@@ -91,58 +113,39 @@ color: #5E5656;
       }
     #Search::placeholder {
         
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 900;
-        font-size: 15px;
+        
         line-height: 18px;
         display: flex;
-        align-items: center;
+      
         margin-left: 20px;
-        color: rgba(69, 68, 68, 0.5);
+        
     
     }
     .bi{
-    position: absolute;
+   
     left: 26px;
     top: 31px;
     bottom: 94.81%;
     font-size: 25px;
-    background: #5E5656;
-    color: #5E5656;
-        &:hover{
-            color: #9A9191;
-        }
+  
     }
 
 
 }
-@media (max-width: 768px){
+@media (max-width: 880px){
 
     input{
-        position: absolute;
+        
         width: 400px;
         height: 45px;
         left: 6px;
         top: 21px;
-        background: #D9D9D9;
-        border-radius: 61px;
-        padding: 0px 45px;
-        border: none;
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 900;
         font-size: 15px;
         line-height: 18px;
-        &:hover{
-            border: none;
-        }
+        
     }
     #Search::-webkit-search-cancel-button {
-        position: relative;
-        background-image: url(cancel.svg);
-        background-repeat: no-repeat;
-        -webkit-appearance: none;
+    
         height: 15px;
         width: 15px;
         left: 20px;
@@ -150,29 +153,59 @@ color: #5E5656;
       }
     #Search::placeholder {
         
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 900;
+     
         font-size: 15px;
         line-height: 18px;
-        display: flex;
-        align-items: center;
         margin-left: 20px;
-        color: rgba(69, 68, 68, 0.5);
+      
     
     }
     .bi{
-    position: absolute;
+  
     left: 18px;
     top: 31px;
     bottom: 94.81%;
     font-size: 25px;
-    background: #5E5656;
-    color: #5E5656;
-        &:hover{
-            color: #9A9191;
-        }
+  
+
+}
+@media (max-width: 425px){
+
+    input{
+        
+        width: 200px;
+        height: 35px;
+        rigth: 90px;
+        left: -45px;
+        top: 21px;
+        font-size: 9px;
+        line-height: 18px;
+        padding: 0px 30px;
+        right: 9px;
+        
     }
+    #Search::-webkit-search-cancel-button {
+    
+        height: 13px;
+        width: 13px;
+        left: 20px;
+     
+      }
+    #Search::placeholder {
+        
+     
+        font-size: 10px;
+        line-height: 18px;
+        margin-left: 20px;
+      
+    
+    }
+    .bi{
+  
+    left: -35px;
+    top: 28px;
+    font-size: 15px;
+  
 
 }
 `;
