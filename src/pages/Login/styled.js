b9e365ec15d80c7bce950d60a89d30bd7c6bd894
Guilherme138 <70076616+Guilherme138@users.noreply.github.com>
2022-05-01 14:48:11 -0300

    update

--- src/pages/Login/styled.js
+++ src/pages/Login/styled.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
-
+import px2vw from "../../utils/px2vw";
 export const AreaLogin = styled.div`
     padding: 30px;
     width:350px;
     margin: auto;
     margin-top: 120px;
     text-align: center;
-    justify-contents: center;
+    justify-content: center;
     height: 395px;
 
 h1{
@@ -108,10 +108,65 @@ input{
     margin-top: 7px;
     margin-bottom: 7px;
     margin-left: -9px;
-    margin-rigth: 7px;
+    margin-right: 7px;
     color: #C4C4C4;
     &:hover{
         color: #9A9191;
     }
 }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: ${px2vw(32)};
+  max-width: 100%;
+
+  @media (min-width: 1024px) {
+    flex-wrap: nowrap;
+  }
+`;
+
+export const Box = styled.div`
+  display: flex;
+  width: ${px2vw(320, 320)};
+  min-height: ${px2vw(200, 320)};
+  flex-direction: column;
+  padding: ${px2vw(20)};
+  margin: ${px2vw(20)};
+  background-color: ${props => props.bgColor};
+  height: 100%;
+
+  @media (min-width: 768px) {
+    width: ${px2vw(320, 768)};
+    min-height: ${px2vw(200, 768)};
+    height: 100%;
+  }
+
+  @media (min-width: 1024px) {
+    width: ${px2vw(500)};
+    min-height: ${px2vw(300)};
+    height: 100%;
+  }
+`;
+
+export const BoxTitle = styled.h3`
+  color: #333;
+  font-size: 2rem;
+  text-align: center;
+
+  @media (min-width: 1024px) {
+    font-size: 1.5rem;
+  }
+`;
+
+export const BoxText = styled.p`
+  margin-top: ${px2vw(20)};
+  color: #666;
+  font-size: 1.5rem;
+
+  @media (min-width: 1024px) {
+    font-size: 1rem;
+  }
 `;
